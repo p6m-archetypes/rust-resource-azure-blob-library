79cfcf25f3ea0223ccd52317f12185a4fb42862c
Jimmie Fulton <jimmie@ybor.ai>
2026-06-05 12:46:31 -0700

    feat: rust-resource-azure-blob-library archetype content (initial import for review)

---
+++ lib/init.lua
@@ -0,0 +1,14 @@
+local M = {}
+
+function M.render(context, opts)
+    opts = opts or {}
+    local d = opts.destination
+    if d and d ~= "" then
+        directory.render("contents", context, { destination = d })
+    else
+        directory.render("contents", context)
+    end
+    return context
+end
+
+return M
